c988f4c20e6a465c301d3000ce08519fed2fe9b6
fjzhang <fjzhang_@outlook.com>
2017-02-26 15:54:43 +0800

    添加配置

---
+++ www/config.py
@@ -0,0 +1,60 @@
+"""
+Configuration
+"""
+
+__author__ = 'fjzhang'
+
+from www import config_default
+
+
+class Dict(dict):
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+def merge(defaults, override):
+    """
+    合并默认配置和覆盖配置
+    :param defaults: 默认配置
+    :param override: 覆盖配置
+    :return:
+    """
+    r = {}
+    for k, v in defaults.items():
+        if k in override:
+            if isinstance(k, dict):
+                r[k] = merge(v, override[k])
+            else:
+                r[k] = override[k]
+        else:
+            r[k] = defaults[k]
+    return r
+
+
+def toDict(d):
+    D = Dict()
+    for k, v in d.items():
+        D[k] = toDict(k) if isinstance(k, dict) else v
+    return D
+
+
+configs = config_default.configs
+
+try:
+    from www import config_override
+    configs = merge(configs, config_override.configs)
+except ImportError:
+    pass
+
+configs = toDict(configs)
---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+"""
+默认配置
+"""
+
+__author__ = 'fjzhang'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': '3306',
+        'user': 'root',
+        'password': 'fjzhang',
+        'db': 'webapp_test'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+
+}
